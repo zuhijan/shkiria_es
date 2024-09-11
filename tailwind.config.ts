1dfb3b2c73d7b029b913d75c80bbb8106f5eca7b
Egor Shkiria <zuhijan@yandex.ru>
2024-09-11 14:58:30 +0100

    init project, clean code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      rotate: {
+        '20': '20deg',
+      }
     },
   },
   plugins: [],
