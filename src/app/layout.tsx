b9393501a8ee0d98f9bfb626884d077fc276ee23
Egor Shkiria <zuhijan@yandex.ru>
2024-11-16 23:15:35 +0000

    add favicon, fix tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {Navbar} from "@/app/components/nav";
 import {ThemeProvider} from "@/app/components/theme-switch";
 import Footer from "@/app/components/footer";
+import {metaData} from "@/app/config";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,9 +17,41 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
 export const metadata: Metadata = {
-  title: "Shkiria",
-  description: "by Egor Shkiria",
+  metadataBase: new URL(metaData.baseUrl),
+  title: {
+    default: metaData.title,
+    template: `%s | ${metaData.title}`,
+  },
+  description: metaData.description,
+  openGraph: {
+    images: metaData.ogImage,
+    title: metaData.title,
+    description: metaData.description,
+    url: metaData.baseUrl,
+    siteName: metaData.name,
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: metaData.name,
+    card: "summary_large_image",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 
@@ -31,34 +64,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cx(geistSans.variable, geistMono.variable)}>
-    <head>
-      <link
-        rel="alternate"
-        type="application/atom+xml"
-        href="/atom.xml"
-        title="Atom Feed"
-      />
-      <link
-        rel="alternate"
-        type="application/feed+json"
-        href="/feed.json"
-        title="JSON Feed"
-      />
-    </head>
-    <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
-        <Navbar />
-        {children}
-        <Footer />
-      </main>
-    </ThemeProvider>
-    </body>
+      <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
+            <Navbar />
+            {children}
+            <Footer />
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    darkMode: "class",
+    content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}", "./public/**/*.svg"],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ["var(--font-geist-sans)"],
+          mono: ["var(--font-geist-mono)"],
+        },
+        typography: {
+          quoteless: {
+            css: {
+              "blockquote p:first-of-type::before": { content: "none" },
+              "blockquote p:first-of-type::after": { content: "none" },
+            },
+          },
+        },
       },
-      rotate: {
-        '20': '20deg',
-      }
     },
-  },
-  plugins: [],
+    plugins: [require("@tailwindcss/typography")],
 };
 export default config;
