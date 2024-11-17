f695d6a2da1192fabd5ab5361e3f6932687339a9
Egor Shkiria <zuhijan@yandex.ru>
2024-11-17 23:08:32 +0000

    change base-url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
+  baseUrl: "https://shkiria-es.vercel.app/",
   title: "Egor Shkiria",
   name: "Egor Shkiria",
   ogImage: "/opengraph-image.png",
